59d7d83c6f832ce871863a66ace44c2749dd8638
shyam kumar <sksingh90405@gmail.com>
2025-03-20 17:09:38 +0530

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
-import React from 'react'
 // import Pagination from './components/pagination/Pagination'
-import DigitalClock from './components/digital_clock'
+// import DigitalClock from './components/digital_clock'
+// import TimerTest from './components/countdown_timer/TimerTest'
+import CountdownTimerTest from './components/countdown_timer/otherVersion/test'
 
 function App() {
   return (
     // <Pagination />
-    <DigitalClock />
+    // <DigitalClock />
+    // <TimerTest />
+    <CountdownTimerTest />
   )
 }
 
